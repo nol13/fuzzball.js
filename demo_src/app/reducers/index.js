f23a5b2ca2eabfbdb680ef392c3c2513f7be7a34
nolan <nolan@nolankaplan.com>
2017-05-21 13:17:40 -0400

    set default demo filter

--- demo_src/app/reducers/index.js
+++ demo_src/app/reducers/index.js
@@ -2,7 +2,7 @@ import { routerReducer as routing } from 'react-router-redux';
 import { combineReducers } from 'redux';
 import * as types from '../actions/types';
 
-const filter = (state = '', action) => {
+const filter = (state = 'aj green', action) => {
     switch (action.type) {
         case types.FILTER:
             return action.filter;
