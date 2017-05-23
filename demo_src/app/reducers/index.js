d91a8fc2fc1a31b0956a53886022d5bbce708d78
nolan <nolan@nolankaplan.com>
2017-05-22 23:27:54 -0400

    set dlc demo query

--- demo_src/app/reducers/index.js
+++ demo_src/app/reducers/index.js
@@ -2,7 +2,7 @@ import { routerReducer as routing } from 'react-router-redux';
 import { combineReducers } from 'redux';
 import * as types from '../actions/types';
 
-const filter = (state = 'aj green', action) => {
+const filter = (state = '542222xx', action) => {
     switch (action.type) {
         case types.FILTER:
             return action.filter;
@@ -29,7 +29,7 @@ const wildcards = (state = '', action) => {
     }
 };
 
-const dataset = (state = 'bengals', action) => {
+const dataset = (state = 'dlc', action) => {
     switch (action.type) {
         case types.DATASET:
             return action.dataset;
