2cf8f5899a3001426e83a9c8a30dfee810dea84f
nolan <nolan@nolankaplan.com>
2017-11-26 23:25:58 -0500

    fix types

--- fuzzball.d.ts
+++ fuzzball.d.ts
@@ -1,22 +1,24 @@
 export interface ExtractResults {
-    [index: number]: [Any, number, string | number];
+    [index: number]: [any, number, string | number];
 }
 
 export interface DedupeResults {
-    [index: number]: [Any, number | string, ExtractResults?];
+    [index: number]: [any, number | string, ExtractResults] | [any, number | string];
 }
 
-export function distance(str1: string, str2: string, opts?: Any): number;
-export function ratio(str1: string, str2: string, opts?: Any): number;
-export function partial_ratio(str1: string, str2: string, opts?: Any): number;
-export function token_set_ratio(str1: string, str2: string, opts?: Any): number;
-export function token_sort_ratio(str1: string, str2: string, opts?: Any): number;
-export function partial_token_set_ratio(str1: string, str2: string, opts?: Any): number;
-export function partial_token_sort_ratio(str1: string, str2: string, opts?: Any): number;
-export function WRatio(str1: string, str2: string, opts?: Any): number;
-export function extract(query: string, choices: Any, opts?: Any): ExtractResults;
-export function extractAsync(query: string, choices: Any, opts?: Any, callback: (err: Any, results?: ExtractResults) => void): void;
-export function full_Process(str: Any, options?: Any | boolean): string;
-export function process_and_sort(str: Any): string;
-export function unique_tokens(str: string, opts?: Any): string[];
-export function dedupe(contains_dupes: Any, opts?: Any): DedupeResults;
+export function distance(str1: string, str2: string, opts?: any): number;
+export function ratio(str1: string, str2: string, opts?: any): number;
+export function partial_ratio(str1: string, str2: string, opts?: any): number;
+export function token_set_ratio(str1: string, str2: string, opts?: any): number;
+export function token_sort_ratio(str1: string, str2: string, opts?: any): number;
+export function partial_token_set_ratio(str1: string, str2: string, opts?: any): number;
+export function partial_token_sort_ratio(str1: string, str2: string, opts?: any): number;
+export function WRatio(str1: string, str2: string, opts?: any): number;
+export function extract(query: string, choices: any, opts?: any): ExtractResults;
+export function extractAsync(query: string, choices: any, opts?: any, callback?: (err: any, results?: ExtractResults) => void): void;
+export function full_Process(str: any, options?: any | boolean): string;
+export function process_and_sort(str: any): string;
+export function unique_tokens(str: string, opts?: any): string[];
+export function dedupe(contains_dupes: any, opts?: any): DedupeResults;
+
+export as namespace fuzzball;
