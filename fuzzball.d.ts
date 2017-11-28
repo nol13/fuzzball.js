5d5745f3df9d466d67e41b63a27c9c7fba60f75b
nolan <nolan@nolankaplan.com>
2017-11-27 19:56:12 -0500

    extracy query can be any

--- fuzzball.d.ts
+++ fuzzball.d.ts
@@ -14,8 +14,8 @@ export function token_sort_ratio(str1: string, str2: string, opts?: any): number
 export function partial_token_set_ratio(str1: string, str2: string, opts?: any): number;
 export function partial_token_sort_ratio(str1: string, str2: string, opts?: any): number;
 export function WRatio(str1: string, str2: string, opts?: any): number;
-export function extract(query: string, choices: any, opts?: any): ExtractResults;
-export function extractAsync(query: string, choices: any, opts?: any, callback?: (err: any, results?: ExtractResults) => void): void;
+export function extract(query: any, choices: any, opts?: any): ExtractResults;
+export function extractAsync(query: any, choices: any, opts?: any, callback?: (err: any, results?: ExtractResults) => void): void;
 export function full_Process(str: any, options?: any | boolean): string;
 export function process_and_sort(str: any): string;
 export function unique_tokens(str: string, opts?: any): string[];
